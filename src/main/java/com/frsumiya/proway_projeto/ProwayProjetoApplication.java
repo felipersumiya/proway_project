54745d59990341b7f35b840468c361c204aeb59a
felipersumiya <felipersumiya@gmail.com>
2021-06-07 16:12:06 -0300

    Projeto criado com sucesso.

---
+++ src/main/java/com/frsumiya/proway_projeto/ProwayProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.frsumiya.proway_projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProwayProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProwayProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frsumiya/proway_projeto/ProwayProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frsumiya.proway_projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProwayProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
